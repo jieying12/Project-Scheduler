6ea4ae97a77ceadcfaa9b3d68673a33ee1c7b3a0
jieying <81208774+jieying12@users.noreply.github.com>
2022-11-18 01:09:03 +0800

    add username property

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,11 @@ const validator = require('validator')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
   email: {
     type: String,
     required: true,
@@ -17,10 +22,10 @@ const userSchema = new Schema({
 })
 
 // static signup method
-userSchema.statics.signup = async function(email, password) {
+userSchema.statics.signup = async function(username, email, password) {
 
   // validation
-  if (!email || !password) {
+  if (!email || !password || !username) {
     throw Error('All fields must be filled')
   }
   if (!validator.isEmail(email)) {
@@ -30,16 +35,22 @@ userSchema.statics.signup = async function(email, password) {
     throw Error('Password not strong enough')
   }
 
-  const exists = await this.findOne({ email })
+  const emailExists = await this.findOne({ email })
 
-  if (exists) {
+  if (emailExists) {
     throw Error('Email already in use')
+  }
+
+  const usernameExists = await this.findOne({ username })
+
+  if (usernameExists) {
+    throw Error('Username already in use')
   }
 
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
 
-  const user = await this.create({ email, password: hash })
+  const user = await this.create({ username, email, password: hash })
 
   return user
 }
