6a9b87b1402b87e94321ec1f95fe079a64d9191e
jieying <81208774+jieying12@users.noreply.github.com>
2022-11-18 12:11:38 +0800

    mui theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AuthContextProvider } from './context/AuthContext'
+import { createTheme, ThemeProvider } from '@mui/material';
+import CssBaseline from "@mui/material/CssBaseline";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#f3dec4"
+    },
+    primary: {//orange
+      main: '#ff7d4b',
+      light: '#ff9b6a',
+      veryLight: '#f3dec4',
+    },
+    secondary: {//blue
+      main: '#1c58b5',
+      light: '#2e74b4',
+      // veryLight: '#2e74b4'
+    },
+    action: {
+      disabledBackground: '#2e74b4',
+      hover: '#2e74b4',
+    }
+  },
+  text: {
+    primary: '#515151',
+    secondary: '#A0A3B1',
+    disabled: '#FFFFFF'
+  },
+  shape: {
+    borderRadius: 12,
+  },
+  typography: {
+    fontFamily: "Poppins, Roboto, Helvetica, Arial, sans-serif",
+    subtitle2: {
+      color: '#A0A3B1'
+    }
+  }
+})
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
