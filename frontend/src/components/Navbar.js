e674715e098610908ff3a7c09735caa6c9994386
Sylvia31082000 <e0439276@u.nus.edu>
2022-11-18 20:25:46 +0800

    added top nav bar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,37 @@
+import { Component } from "react"
+import { FaHome, FaSave, FaInfoCircle, FaBook } from "react-icons/fa"
+import './Navbar.css';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="NavbarItems">
+                <h1 className="logo">Project Scheduler</h1>
+                <ul className="nav-menu">
+                    <li>
+                        <a className="nav-links" href="index.html">
+                            <FaHome/> Home
+                        </a>
+                    </li>
+                    <li>
+                        <a className="nav-links" href="index.html">
+                            <FaSave/> Saved Timetables
+                        </a>
+                    </li>
+                    <li>
+                        <a className="nav-links" href="index.html">
+                            <FaBook/> Past Modules
+                        </a>
+                    </li>
+                    <li>
+                        <a className="nav-links" href="index.html">
+                            <FaInfoCircle/> All Modules
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
